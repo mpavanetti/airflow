83cb887650b5543342344883374d5b454768501b
Matheus Pavanetti <matheuspavanetti@gmail.com>
2022-01-28 20:44:03 +0000

    Importing, libraries, creating variables for storing time, creating variables for reading connections and global variables from airflow database, creating dictionary for storing http query parameters.

--- weather_data.py
+++ weather_data.py
@@ -1,15 +1,33 @@
 # Import default Apache Airflow Libraries
 from airflow.models import DAG
 from airflow.operators.dummy import DummyOperator
+from airflow.hooks.base import BaseHook
+from airflow.models import Variable
 
 
 # Importing Python Libraries
 from datetime import datetime, timedelta
+import time
 
 
 # Default Arguments and attibutes
 default_args ={
     'start_date': datetime.today() - timedelta(days=1)
+}
+
+# Get Current date, subtract 5 days and convert to timestamp
+todayLessFiveDays =  datetime.today() - timedelta(days=5)
+todayLessFiveDaysTimestamp = time.mktime(todayLessFiveDays.timetuple())
+
+# Get Connection from airflow db
+connection = BaseHook.get_connection("openweathermapApi")
+
+# weather data api query params
+api_params = {
+    'lat':Variable.get("weather_data_lat"),
+    'lon':Variable.get("weather_data_lon"),
+    'dt':int(todayLessFiveDaysTimestamp),
+    'appid':connection.password,
 }
 
 # DAG Skeleton
