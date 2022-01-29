c0daeaaf472b4cc76fe5e1cdae48181f4e73c4b9
Matheus Pavanetti <matheuspavanetti@gmail.com>
2022-01-29 21:33:41 +0000

    Importing spark libarries, adding new temp directories, creatinf functions for store_hourly_weather, csv, spark function for processing data, sqlite ingest, dag task and dependency

--- weather_data.py
+++ weather_data.py
@@ -19,6 +19,11 @@ import os
 from pandas import json_normalize
 from geopy.geocoders import Nominatim
 import csv, sqlite3
+import glob
+
+# Spark Imports
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col,explode, element_at, expr,unix_timestamp, to_timestamp, to_date, regexp_replace
 
 
 # Default Arguments and attibutes
@@ -58,6 +63,12 @@ def _tmp_data():
     # Checking if directories exist
     if not os.path.exists(tmp_data_dir):
         os.mkdir(tmp_data_dir)
+    if not os.path.exists(f'{tmp_data_dir}/hourly_weather/'):
+        os.mkdir(f'{tmp_data_dir}/hourly_weather/')
+    if not os.path.exists(f'{tmp_data_dir}processed/'):
+        os.mkdir(f'{tmp_data_dir}processed/')
+    if not os.path.exists(f'{tmp_data_dir}processed/hourly_weather/'):
+        os.mkdir(f'{tmp_data_dir}processed/hourly_weather/')
         
 # create a database connection to the SQLite database specified by db_file    
 def create_connection():
@@ -153,8 +164,69 @@ def _store_requested_weather_sqlite():
     reader = csv.reader(open(f'{tmp_data_dir}requested.csv'))
     cur.executemany('INSERT OR IGNORE INTO requested_weather VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',reader)
     conn.commit()
+    
+# Store Hourly Weather CSV
+def _store_hourly_weather_csv(ti):
+    weather= store_weather_response(ti)
+    time = str(weather['current']['dt'])
+    
+    # Write dictonary/json into file for futher processing.
+    with open(f"{tmp_data_dir}/hourly_weather/hourly_weather_{time}.json", "w") as outfile:
+     json.dump(weather, outfile)
+    
+# Spark Process hourly weather
+def _spark_process_hourly_weather():
+    spark = SparkSession \
+      .builder  \
+      .appName("hourly_data")  \
+      .getOrCreate()
+      
+    df = spark.read.format("json") \
+            .option('inferSchema',True) \
+            .load(f'{tmp_data_dir}/hourly_weather/') \
+            .drop("current","timezone_offset")
 
+    df_stg = df.withColumn('hourly',explode(col('hourly'))) \
+                .withColumn("datetime", to_timestamp(expr("hourly.dt")))    \
+                .withColumn("temp", expr("hourly.temp")) \
+                .withColumn("feels_like", expr("hourly.feels_like")) \
+                .withColumn("pressure", expr("hourly.pressure")) \
+                .withColumn("humidity", expr("hourly.humidity")) \
+                .withColumn("dew_point", expr("hourly.dew_point")) \
+                .withColumn("uvi", expr("hourly.uvi")) \
+                .withColumn("clouds", expr("hourly.clouds")) \
+                .withColumn("visibility", expr("hourly.visibility")) \
+                .withColumn("wind_speed", expr("hourly.wind_speed")) \
+                .withColumn("wind_deg", expr("hourly.wind_deg")) \
+                .withColumn("wind_gust", expr("hourly.wind_gust")) \
+                .withColumn("weather_id", expr("hourly.weather.id")) \
+                .withColumn("weather_id", element_at(col("weather_id"), 1)) \
+                .withColumn("weather_main", expr("hourly.weather.main")) \
+                .withColumn("weather_main", element_at(col("weather_main"), 1)) \
+                .withColumn("weather_description", expr("hourly.weather.description")) \
+                .withColumn("weather_description", element_at(col("weather_description"), 1)) \
+                .withColumn("weather_icon", expr("hourly.weather.icon")) \
+                .withColumn("weather_icon", element_at(col("weather_icon"), 1)) \
+                .drop("hourly")
+                
+    final_df = df_stg.withColumnRenamed('lat','latitude') \
+                .withColumnRenamed('lon','longitude') \
+                .coalesce(1)
+                
+    final_df.write \
+    .format('csv') \
+    .mode('overwrite') \
+    .option('header',False) \
+    .option('sep',',') \
+    .save(f'{tmp_data_dir}processed/hourly_weather/')
 
+def _store_hourly_processed_csv_to_sqlite():
+    conn = create_connection()
+    cur = conn.cursor()
+    reader = csv.reader(open(glob.glob(f'{tmp_data_dir}processed/hourly_weather/part-*.csv')[0]))
+    cur.executemany('INSERT OR IGNORE INTO hourly_weather VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',reader)
+    conn.commit()
+    
 # DAG Skeleton
 with DAG('weather_data', schedule_interval='@daily',default_args=default_args, catchup=False) as dag:
     
@@ -256,6 +328,35 @@ with DAG('weather_data', schedule_interval='@daily',default_args=default_args, c
                 );
                 '''
         )
+        
+        creating_table_hourly_weather = SqliteOperator(
+            task_id='creating_table_hourly_weather',
+            sqlite_conn_id='db_sqlite',
+            sql='''
+                CREATE TABLE IF NOT EXISTS hourly_weather (
+                    latitude TEXT NOT NULL,
+                    longitude TEXT NOT NULL,
+                    timezone TEXT NOT NULL,
+                    datetime TEXT NULL,
+                    temp TEXT NULL,
+                    feels_like TEXT NULL,
+                    pressure TEXT NULL,
+                    humidity TEXT NULL,
+                    dew_point TEXT NULL,
+                    uvi TEXT NULL,
+                    clouds TEXT NULL,
+                    visibility TEXT NULL,
+                    wind_speed TEXT NULL,
+                    wind_deg TEXT NULL,
+                    wind_gust TEXT NULL,
+                    weather_id TEXT NULL,
+                    weather_main TEXT NULL,
+                    weather_description TEXT NULL,
+                    weather_icon TEXT NULL,
+                    PRIMARY KEY (latitude,longitude,datetime)
+                );
+                '''
+        )
     
     # TaskGroup for Processing Data
     with TaskGroup('processing_data') as processing_data:
@@ -272,6 +373,12 @@ with DAG('weather_data', schedule_interval='@daily',default_args=default_args, c
             python_callable=_store_requested_weather_csv
         )
         
+        # Store hourly Weather Data
+        store_hourly_weather_csv = PythonOperator(
+            task_id='store_hourly_weather_csv',
+            python_callable=_store_hourly_weather_csv
+        )
+        
     # TaskGroup for Storing CSV Files into SQLITE tables
     with TaskGroup('storing_csv_to_sqlite') as storing_csv_to_sqlite:
         
@@ -284,6 +391,20 @@ with DAG('weather_data', schedule_interval='@daily',default_args=default_args, c
             task_id='store_requested_weather_sqlite',
             python_callable=_store_requested_weather_sqlite
         )
+        
+    # TaskGroup for Spark Processors
+    with TaskGroup('spark_processors') as spark_processors:
+        spark_process_hourly_weather=PythonOperator(
+            task_id='spark_process_hourly_weather',
+            python_callable=_spark_process_hourly_weather
+        )
+        
+    # TaskGroup for Spark Processors
+    with TaskGroup('store_processed_data_sqlite') as store_processed_data_sqlite:
+        store_hourly_processed_csv_to_sqlite=PythonOperator(
+            task_id='store_hourly_processed_csv_to_sqlite',
+            python_callable=_store_hourly_processed_csv_to_sqlite
+        )
     
     # Cleanup task    
     cleanup= BashOperator(
@@ -294,5 +415,4 @@ with DAG('weather_data', schedule_interval='@daily',default_args=default_args, c
     
     # DAG Dependencies
     start >> tmp_data >> check_api >> [extracting_weather,api_not_available]
-    extracting_weather >> create_sqlite_tables >> processing_data >> storing_csv_to_sqlite >> cleanup
-    
+    extracting_weather >> create_sqlite_tables >> processing_data >> storing_csv_to_sqlite >> spark_processors >> store_processed_data_sqlite >> cleanup
